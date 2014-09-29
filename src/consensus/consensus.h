e6b512523b4366b12b69ed2b3e5e00df22f31121
Peter Todd <pete@petertodd.org>
2014-09-29 01:02:59 -0400

    Move LOCKTIME_THRESHOLD to src/script/script.h
    Will now be needed by CHECKLOCKTIMEVERIFY code.
    
    Rebased-From: 48e9c57cf06352f890eac4285ae022d8746cf3fd

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -8,7 +8,5 @@
 
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
 static const int COINBASE_MATURITY = 100;
-/** Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp. */
-static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
 
 #endif // BITCOIN_CONSENSUS_CONSENSUS_H
