ac57e309e85f7e8f886bbb62f63fc504d0682591
Jonas Schnelli <jonas.schnelli@include7.ch>
2014-12-16 10:50:44 +0100

    [REST] add JSON support for /rest/headers/

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -78,7 +78,6 @@ UniValue blockheaderToJSON(const CBlockIndex* blockindex)
     return result;
 }
 
-
 UniValue blockToJSON(const CBlock& block, const CBlockIndex* blockindex, bool txDetails = false)
 {
     UniValue result(UniValue::VOBJ);
@@ -119,7 +118,6 @@ UniValue blockToJSON(const CBlock& block, const CBlockIndex* blockindex, bool tx
         result.push_back(Pair("nextblockhash", pnext->GetBlockHash().GetHex()));
     return result;
 }
-
 
 UniValue getblockcount(const UniValue& params, bool fHelp)
 {
