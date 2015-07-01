808096e54e7069956819ceee68c97d65886ab6a6
Wladimir J. van der Laan <laanwj@gmail.com>
2015-07-01 17:38:15 +0200

    Use real number of cores for default -par, ignore virtual cores
    To determine the default for `-par`, the number of script verification
    threads, use [boost::thread::physical_concurrency()](http://www.boost.org/doc/libs/1_58_0/doc/html/thread/thread_management.html#thread.thread_management.thread.physical_concurrency)
    which counts only physical cores, not virtual cores.
    
    Virtual cores are roughly a set of cached registers to avoid context
    switches while threading, they cannot actually perform work, so spawning
    a verification thread for them could even reduce efficiency and will put
    undue load on the system.
    
    Should fix issue #6358, as well as some other reported system overload
    issues, especially on Intel processors.
    
    The function was only introduced in boost 1.56, so provide a utility
    function `GetNumCores` to fall back for older Boost versions.
    
    Conflicts:
    	src/init.cpp
    	src/miner.cpp

--- src/init.cpp
+++ src/init.cpp
@@ -289,7 +289,7 @@ std::string HelpMessage(HelpMessageMode mode)
     strUsage += HelpMessageOpt("-maxmempool=<n>", strprintf(_("Keep the transaction memory pool below <n> megabytes (default: %u)"), DEFAULT_MAX_MEMPOOL_SIZE));
     strUsage += HelpMessageOpt("-mempoolexpiry=<n>", strprintf(_("Do not keep transactions in the mempool longer than <n> hours (default: %u)"), DEFAULT_MEMPOOL_EXPIRY));
     strUsage += HelpMessageOpt("-par=<n>", strprintf(_("Set the number of script verification threads (%u to %d, 0 = auto, <0 = leave that many cores free, default: %d)"),
-        -(int)boost::thread::hardware_concurrency(), MAX_SCRIPTCHECK_THREADS, DEFAULT_SCRIPTCHECK_THREADS));
+        -GetNumCores(), MAX_SCRIPTCHECK_THREADS, DEFAULT_SCRIPTCHECK_THREADS));
 #ifndef WIN32
     strUsage += HelpMessageOpt("-pid=<file>", strprintf(_("Specify pid file (default: %s)"), "bitcoind.pid"));
 #endif
