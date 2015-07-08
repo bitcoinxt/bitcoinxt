3735c9332ce40f7e1e3f451440212cd887a5d3ee
Wladimir J. van der Laan <laanwj@gmail.com>
2015-07-08 14:47:39 +0200

    rpc: Remove chain-specific RequireRPCPassword
    I've never liked the chain-specific exception to having to set a
    password. It gives issues with #6388 which makes it valid to
    set no password in every case (as it enables random cookie authentication).
    
    This pull removes the flag, so that all chains are regarded the same.
    
    It also removes the username==password test, which doesn't provide any
    substantial extra security.

--- src/chainparams.h
+++ src/chainparams.h
@@ -50,7 +50,6 @@ public:
     int GetDefaultPort() const { return nDefaultPort; }
 
     const CBlock& GenesisBlock() const { return genesis; }
-    bool RequireRPCPassword() const { return fRequireRPCPassword; }
     /** Make miner wait to have peers to avoid wasting work */
     bool MiningRequiresPeers() const { return fMiningRequiresPeers; }
     /** Default value for -checkmempool and -checkblockindex argument */
@@ -80,7 +79,6 @@ protected:
     std::string strNetworkID;
     CBlock genesis;
     std::vector<SeedSpec6> vFixedSeeds;
-    bool fRequireRPCPassword;
     bool fMiningRequiresPeers;
     bool fDefaultConsistencyChecks;
     bool fRequireStandard;
