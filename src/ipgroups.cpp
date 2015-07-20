bitcoin/bitcoin

Introduce the beginnings of anti-DoS resource scheduling.
Peers now have a priority that attempts to estimate their importance. Currently it is just based on IP address. The default score is zero. In future it may take into account things like how many blocks were relayed, etc. When a node reaches its max connection slots, it will attempt to find a peer with a lower priority than the one trying to connect and disconnect it, to stay below the max connection limit.

Peer priorities are based on matching the connecting IP against a set of IP groups. For now, the only IP group is one that gives Tor exits a score of -10. This is to address DoS attacks that are being reported on the main network in which an attacker builds many connections via Tor to use up all the connection slots and jam the node for clearnet users. It's a more robust approach than simply banning abused proxies altogether.

For simplicity, a generated list of exits is compiled into the binary. A future enhancement would be to load IP groups from an external file and/or load from HTTPS urls (would require a new dependency on cpp-netlib).

Other anonymizing proxy networks that are attractive to DoS attackers may also be added as alternative IP groups, as a quick fix. Eventually peer priority can be calculated in a more free floating and dynamic manner and the hardcoded IP approach may become unneeded.

src/ipgroups.cpp
@@ -1,0 +1,46 @@
+// Copyright (c) 2009-2015 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <boost/foreach.hpp>
+#include "ipgroups.h"
+#include "sync.h"
+
+
+#include "torips.cpp"
+#include "util.h"
+
+static CCriticalSection cs_groups;
+static std::vector<CIPGroup*> groups;
+
+// Returns NULL if the IP does not belong to any group.
+CIPGroup *FindGroupForIP(CNetAddr ip) {
+    if (!ip.IsValid()) {
+        LogPrintf("IP is not valid: %s\n", ip.ToString());
+        return NULL;
+    }
+
+    LOCK(cs_groups);
+    BOOST_FOREACH(CIPGroup *group, groups)
+    {
+        BOOST_FOREACH(const CSubNet &subNet, group->subnets)
+        {
+            if (subNet.Match(ip))
+                return group;
+        }
+    }
+    return NULL;
+}
+
+void InitIPGroups() {
+    // Load Tor Exits as a group.
+    CIPGroup *ipGroup = new CIPGroup("tor");
+    ipGroup->priority = -10;
+    for (const char **ptr = pszTorExits; *ptr; ptr++) {
+        std::string ip(*ptr);
+        ip = ip + "/32";
+        ipGroup->subnets.push_back(CSubNet(ip));
+    }
+    LOCK(cs_groups);
+    groups.push_back(ipGroup);   // Deliberately leak it.
+}

bitcoinxt/bitcoinxt

src/ipgroups.cpp
@@ -1,0 +1,45 @@
+// Copyright (c) 2009-2015 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <boost/foreach.hpp>
+#include "ipgroups.h"
+#include "sync.h"
+
+#include "torips.h"
+#include "util.h"
+
+static CCriticalSection cs_groups;
+static std::vector<CIPGroup*> groups;
+
+// Returns NULL if the IP does not belong to any group.
+CIPGroup *FindGroupForIP(CNetAddr ip) {
+    if (!ip.IsValid()) {
+        LogPrintf("IP is not valid: %s\n", ip.ToString());
+        return NULL;
+    }
+
+    LOCK(cs_groups);
+    BOOST_FOREACH(CIPGroup *group, groups)
+    {
+        BOOST_FOREACH(const CSubNet &subNet, group->subnets)
+        {
+            if (subNet.Match(ip))
+                return group;
+        }
+    }
+    return NULL;
+}
+
+void InitIPGroups() {
+    // Load Tor Exits as a group.
+    CIPGroup *ipGroup = new CIPGroup("tor");
+    ipGroup->priority = -10;
+    for (const char **ptr = pszTorExits; *ptr; ptr++) {
+        std::string ip(*ptr);
+        ip = ip + "/32";
+        ipGroup->subnets.push_back(CSubNet(ip));
+    }
+    LOCK(cs_groups);
+    groups.push_back(ipGroup);   // Deliberately leak it.
+}