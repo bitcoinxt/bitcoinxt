25d7ba07712ee25e6ceae94aae200aefc110a036
mruddy <mruddy@users.noreply.github.com>
2015-08-16 14:25:42 -0400

    MIN_PEER_PROTO_VERSION and p2p network protocol version related code cleanup

--- src/version.h
+++ src/version.h
@@ -5,33 +5,13 @@
 #ifndef BITCOIN_VERSION_H
 #define BITCOIN_VERSION_H
 
-/**
- * network protocol versioning
- */
+// disconnect from peers older than this protocol version.
+static const int MIN_PEER_PROTO_VERSION = 70010;
 
-static const int PROTOCOL_VERSION = 70010;
+// p2p network protocol version that this node implements.
+static const int PROTOCOL_VERSION = MIN_PEER_PROTO_VERSION + 0;
 
-//! initial proto version, to be increased after version/verack negotiation
-static const int INIT_PROTO_VERSION = 209;
-
-//! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 31800;
-
-//! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = GETHEADERS_VERSION;
-
-//! nTime field added to CAddress, starting with this version;
-//! if possible, avoid requesting addresses nodes older than this
-static const int CADDR_TIME_VERSION = 31402;
-
-//! only request blocks from nodes outside this range of versions
-static const int NOBLKS_VERSION_START = 32000;
-static const int NOBLKS_VERSION_END = 32400;
-
-//! BIP 0031, pong message, is enabled for all versions AFTER this one
-static const int BIP0031_VERSION = 60000;
-
-//! "mempool" command, enhanced "getdata" behavior starts with this version
-static const int MEMPOOL_GD_VERSION = 60002;
+// initial protocol version used between peers. the version used between peers may be increased by version/verack negotiation.
+static const int INIT_PROTO_VERSION = MIN_PEER_PROTO_VERSION;
 
 #endif // BITCOIN_VERSION_H
