71754a2726dcd9a98e4183a32aa70976138e3f28
mruddy <mruddy@users.noreply.github.com>
2015-10-28 13:19:12 -0400

    set INIT_PROTO_VERSION back to 209

--- src/version.h
+++ src/version.h
@@ -12,6 +12,6 @@ static const int MIN_PEER_PROTO_VERSION = 70010;
 static const int PROTOCOL_VERSION = MIN_PEER_PROTO_VERSION + 0;
 
 // initial protocol version used between peers. the version used between peers may be increased by version/verack negotiation.
-static const int INIT_PROTO_VERSION = MIN_PEER_PROTO_VERSION;
+static const int INIT_PROTO_VERSION = 209;
 
 #endif // BITCOIN_VERSION_H
