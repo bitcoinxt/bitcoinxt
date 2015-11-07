bitcoinxt/bitcoinxt

commit df05702b3fd0a2ae9e40b9ff41e476cadf19885c

Divides -maxmempoolbytes by 2.75; removed debugging lines

src/main.cpp
@@ -25,7 +25,6 @@
 #include "validationinterface.h"
 
 #include <sstream>
-#include <boost/chrono.hpp>
 
 #include <boost/dynamic_bitset.hpp>
 #include <boost/algorithm/string/replace.hpp>
@@ -1198,7 +1197,8 @@ bool AcceptToMemoryPool(CTxMemPool& pool, CValidationState &state, const CTransa
 
             // Default max mempool size: 50 blocks-worth of transactions
             int64_t nDefaultMaxBytes = 50 * (int64_t)(maxBlockSize);
-            int64_t nMaxPoolBytes = GetArg("-maxmempoolbytes", nDefaultMaxBytes);
+            // approx 2.75 in-memory bytes per serialized tx byte
+            int64_t nMaxPoolBytes = GetArg("-maxmempoolbytes", nDefaultMaxBytes) * 100 / 275; 
 
             if (nMaxPoolTx <= 0) { // Zero or negative: don't limit
                 pool.addUnchecked(hash, entry, !IsInitialBlockDownload());
@@ -1242,9 +1242,6 @@ bool AcceptToMemoryPool(CTxMemPool& pool, CValidationState &state, const CTransa
                     return false;
             }
             if (nMaxPoolBytes > 0 && (int64_t)pool.GetTotalTxSize() > nMaxPoolBytes) {
-                boost::chrono::high_resolution_clock::time_point t1;
-                boost::chrono::high_resolution_clock::time_point t2;
-                t1 = boost::chrono::high_resolution_clock::now();
                 list<CTransaction> evicted;
                 pool.evictRandomBytewise(evicted);
                 BOOST_FOREACH(const CTransaction &etx, evicted) {
@@ -1253,9 +1250,6 @@ bool AcceptToMemoryPool(CTxMemPool& pool, CValidationState &state, const CTransa
                         etx.GetHash().ToString());
                     SyncWithWallets(etx, NULL, false);
                 }
-                t2 = boost::chrono::high_resolution_clock::now();
-                boost::chrono::nanoseconds ns = boost::chrono::duration_cast<boost::chrono::nanoseconds>(t2-t1);
-                LogPrint("mempool", "Bytewise evictions took %lli ns\n", ((long long)(ns.count())));
                 if (!pool.exists(hash))
                     return false;
 

src/init.cpp
@@ -315,7 +315,7 @@ std::string HelpMessage(HelpMessageMode mode)
     strUsage += HelpMessageOpt("-maxreceivebuffer=<n>", strprintf(_("Maximum per-connection receive buffer, <n>*1000 bytes (default: %u)"), 5000));
     strUsage += HelpMessageOpt("-maxsendbuffer=<n>", strprintf(_("Maximum per-connection send buffer, <n>*1000 bytes (default: %u)"), 1000));
     strUsage += HelpMessageOpt("-maxmempooltx=<n>", ("Maximum number of transactions in memory pool (default: enough to fill about 25 blocks with 500 byte transactions)"));
-    strUsage += HelpMessageOpt("-maxmempoolbytes=<n>", ("Maximum size (in bytes) of transactions in memory pool (default: enough to fill 50 blocks)"));
+    strUsage += HelpMessageOpt("-maxmempoolbytes=<n>", ("Maximum in-memory size of transactions in memory pool (default: enough to fill 50 blocks)"));
     strUsage += HelpMessageOpt("-onion=<ip:port>", strprintf(_("Use separate SOCKS5 proxy to reach peers via Tor hidden services (default: %s)"), "-proxy"));
     strUsage += HelpMessageOpt("-onlynet=<net>", _("Only connect to nodes in network <net> (ipv4, ipv6 or onion)"));
     strUsage += HelpMessageOpt("-permitbaremultisig", strprintf(_("Relay non-P2SH multisig (default: %u)"), 1));

src/txmempool.cpp
@@ -276,11 +276,6 @@ void CTxMemPool::evictRandomBytewise(std::list<CTransaction>& evicted)
         bool pr1 = (mapDeltas.find(tx1.first) != mapDeltas.end());
         bool pr2 = (mapDeltas.find(tx2.first) != mapDeltas.end());
 
-        LogPrint("mempool", "Eviction candidate 1: %i byte %i satoshi %s\n", 
-                        tx1.second.GetTxSize(), tx1.second.GetFee(), tx1.first.ToString());
-        LogPrint("mempool", "Eviction candidate 2: %i byte %i satoshi %s\n", 
-                        tx2.second.GetTxSize(), tx2.second.GetFee(), tx2.first.ToString());
-
         if (pr1 && pr2) continue; // prioritized, try again
         if (!tx1.second.GetTxSize() || !tx2.second.GetTxSize()) continue; // probably unnecessary
 

commit eca6b758a3c51233682fc751149c8294e54379d4

- Fixed maxmempoolbytes default value bug introduced by df05702

src/main.cpp
@@ -1196,7 +1196,7 @@ bool AcceptToMemoryPool(CTxMemPool& pool, CValidationState &state, const CTransa
             int64_t nMaxPoolTx = GetArg("-maxmempooltx", nDefaultMax);
 
             // Default max mempool size: 50 blocks-worth of transactions
-            int64_t nDefaultMaxBytes = 50 * (int64_t)(maxBlockSize);
+            int64_t nDefaultMaxBytes = 50 * (int64_t)(maxBlockSize) * 100 / 275;
             // approx 2.75 in-memory bytes per serialized tx byte
             int64_t nMaxPoolBytes = GetArg("-maxmempoolbytes", nDefaultMaxBytes) * 100 / 275; 
 