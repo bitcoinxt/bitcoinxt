3a7c2ae95b24aa68ef4f15b053e69dcda9fa7fac
Wladimir J. van der Laan <laanwj@gmail.com>
2015-11-09 08:40:46 +0100

    Fix crash in validateaddress with -disablewallet
    Fix a null pointer dereference in validateaddress with -disablewallet. Also add a regression testcase.

---
+++ qa/rpc-tests/disablewallet.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# Copyright (c) 2015 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#
+# Exercise API with -disablewallet.
+#
+
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import *
+
+class DisableWalletTest (BitcoinTestFramework):
+
+    def __init__(self):
+        super().__init__()
+        self.setup_clean_chain = True
+        self.num_nodes = 1
+
+    def setup_network(self, split=False):
+        self.nodes = start_nodes(self.num_nodes, self.options.tmpdir, [['-disablewallet']])
+        self.is_network_split = False
+        self.sync_all()
+
+    def run_test (self):
+        # Check regression: https://github.com/bitcoin/bitcoin/issues/6963#issuecomment-154548880
+        x = self.nodes[0].validateaddress('3J98t1WpEZ73CNmQviecrnyiWrnqRhWNLy')
+        assert(x['isvalid'] == False)
+        x = self.nodes[0].validateaddress('mneYUmWYsuk7kySiURxCi3AGxrAqZxLgPZ')
+        assert(x['isvalid'] == True)
+
+if __name__ == '__main__':
+    DisableWalletTest ().main ()
