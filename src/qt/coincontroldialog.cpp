25c415c1f0bb485215cc573b69cd27fe940a9c69
João Barbosa <joao@uphold.com>
2016-02-11 01:07:22 +0000

    Use CCoinControl selection in CWallet::FundTransaction

--- src/qt/coincontroldialog.cpp
+++ src/qt/coincontroldialog.cpp
@@ -795,7 +795,7 @@ void CoinControlDialog::updateView()
             }
 
             // set checkbox
-            if (coinControl->IsSelected(txhash, out.i))
+            if (coinControl->IsSelected(COutPoint(txhash, out.i)))
                 itemOutput->setCheckState(COLUMN_CHECKBOX, Qt::Checked);
         }
 
