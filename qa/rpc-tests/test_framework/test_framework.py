ca0b10e083f5041b667c1e61195c435f771164d1
MarcoFalke <falke.marco@gmail.com>
2016-04-27 22:29:52 +0200

    [qa] Refactor test_framework and pull tester
    * log to stdout
    * increase range for p2p and rpc ports
    * UPPERCASE_CONSTANTS
    * Stop nodes on CTRL+C

--- qa/rpc-tests/test_framework/test_framework.py
+++ qa/rpc-tests/test_framework/test_framework.py
@@ -124,7 +124,7 @@ class BitcoinTestFramework(object):
 
         if self.options.trace_rpc:
             import logging
-            logging.basicConfig(level=logging.DEBUG)
+            logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 
         if self.options.coveragedir:
             enable_coverage(self.options.coveragedir)
@@ -154,6 +154,8 @@ class BitcoinTestFramework(object):
         except Exception as e:
             print("Unexpected exception caught during testing: "+str(e))
             traceback.print_tb(sys.exc_info()[2])
+        except KeyboardInterrupt as e:
+            print("Exiting after " + repr(e))
 
         if not self.options.noshutdown:
             print("Stopping nodes")
