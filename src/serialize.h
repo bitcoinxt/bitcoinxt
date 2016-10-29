4edccbda1b689d294749ad59ce50a078052f4ea5
Pieter Wuille <pieter.wuille@gmail.com>
2016-10-28 17:50:04 -0700

    Add optimized CSizeComputer serializers
    To get the advantages of faster GetSerializeSize() implementations
    back that were removed in "Make GetSerializeSize a wrapper on top of
    CSizeComputer", reintroduce them in the few places in the form of a
    specialized Serialize() implementation. This actually gets us in a
    better state than before, as these even get used when they're invoked
    indirectly in the serialization of another object.

--- src/serialize.h
+++ src/serialize.h
@@ -152,6 +152,8 @@ inline float ser_uint32_to_float(uint32_t y)
 // i.e. anything that supports .read(char*, size_t) and .write(char*, size_t)
 //
 
+class CSizeComputer;
+
 enum
 {
     // primary actions
@@ -224,6 +226,8 @@ inline unsigned int GetSizeOfCompactSize(uint64_t nSize)
     else if (nSize <= std::numeric_limits<unsigned int>::max())  return sizeof(unsigned char) + sizeof(unsigned int);
     else                         return sizeof(unsigned char) + sizeof(uint64_t);
 }
+
+inline void WriteCompactSize(CSizeComputer& os, uint64_t nSize);
 
 template<typename Stream>
 void WriteCompactSize(Stream& os, uint64_t nSize)
@@ -318,6 +322,9 @@ inline unsigned int GetSizeOfVarInt(I n)
     }
     return nRet;
 }
+
+template<typename I>
+inline void WriteVarInt(CSizeComputer& os, I n);
 
 template<typename Stream, typename I>
 void WriteVarInt(Stream& os, I n)
@@ -718,6 +725,17 @@ inline void SerReadWrite(Stream& s, T& obj, CSerActionUnserialize ser_action)
 
 
 
+/* ::GetSerializeSize implementations
+ *
+ * Computing the serialized size of objects is done through a special stream
+ * object of type CSizeComputer, which only records the number of bytes written
+ * to it.
+ *
+ * If your Serialize or SerializationOp method has non-trivial overhead for
+ * serialization, it may be worthwhile to implement a specialized version for
+ * CSizeComputer, which uses the s.seek() method to record bytes that would
+ * be written instead.
+ */
 class CSizeComputer
 {
 protected:
@@ -729,6 +747,12 @@ public:
     CSizeComputer(int nTypeIn, int nVersionIn) : nSize(0), nType(nTypeIn), nVersion(nVersionIn) {}
 
     void write(const char *psz, size_t _nSize)
+    {
+        this->nSize += _nSize;
+    }
+
+    /** Pretend _nSize bytes are written, without specifying them. */
+    void seek(size_t _nSize)
     {
         this->nSize += _nSize;
     }
@@ -747,6 +771,17 @@ public:
     int GetVersion() const { return nVersion; }
     int GetType() const { return nType; }
 };
+
+template<typename I>
+inline void WriteVarInt(CSizeComputer &s, I n)
+{
+    s.seek(GetSizeOfVarInt<I>(n));
+}
+
+inline void WriteCompactSize(CSizeComputer &s, uint64_t nSize)
+{
+    s.seek(GetSizeOfCompactSize(nSize));
+}
 
 template <typename T>
 size_t GetSerializeSize(const T& t, int nType, int nVersion = 0)
