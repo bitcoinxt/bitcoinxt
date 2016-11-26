31a4ad317aa91ef4d27fdf7c3eca14d2f6f3ae51
Matt Corallo <git@bluematt.me>
2016-11-25 19:29:55 -0800

    Fix unlocked access to vNodes.size()

--- src/net.cpp
+++ src/net.cpp
@@ -829,8 +829,13 @@ void CConnman::ThreadSocketHandler()
                 }
             }
         }
-        if(vNodes.size() != nPrevNodeCount) {
-            nPrevNodeCount = vNodes.size();
+        size_t vNodesSize;
+        {
+            LOCK(cs_vNodes);
+            vNodesSize = vNodes.size();
+        }
+        if(vNodesSize != nPrevNodeCount) {
+            nPrevNodeCount = vNodesSize;
             if(clientInterface)
                 clientInterface->NotifyNumConnectionsChanged(nPrevNodeCount);
         }
