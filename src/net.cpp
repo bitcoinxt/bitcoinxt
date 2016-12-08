7ca21e9b43853e5da22973f8d0e6ec32fb2ca5b9
R E Broadley <rebroad+github@gmail.com>
2016-12-08 16:34:30 +0700

    Don't ignore nodes advertising SEGWIT

--- src/net.cpp
+++ src/net.cpp
@@ -2203,9 +2203,5 @@ bool CNode::SupportsXThinBlocks() const {
 }
 
 bool CNode::SupportsCompactBlocks() const {
-
-    // If a Core node activates segwit, it also
-    // disables support for compact blocks v1
-    return nVersion >= SHORT_IDS_BLOCKS_VERSION
-        && !(nServices & NODE_WITNESS);
+    return nVersion >= SHORT_IDS_BLOCKS_VERSION;
 }
