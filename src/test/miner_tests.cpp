97178a0668085d01740e71d1ca21d8f09724cc3a
Dagur Valberg Johannsson <dagurval@pvv.ntnu.no>
2017-01-30 18:02:19 -0500

    BIP100: Unit and regression tests
    Unit tests for GetNextMaxBlockSize and FindVote.
    Multi-node voting regression test.
    
    With Tom Harding <tomh@thinlink.com>
    Includes regtest routines by Gavin Andresen <gavinandresen@gmail.com>

--- src/test/miner_tests.cpp
+++ src/test/miner_tests.cpp
@@ -9,6 +9,7 @@
 #include "pubkey.h"
 #include "uint256.h"
 #include "util.h"
+#include "options.h"
 
 #include "test/test_bitcoin.h"
 
@@ -379,6 +380,40 @@ BOOST_AUTO_TEST_CASE(CreateNewBlock_validity)
         delete tx;
 
     fCheckpointsEnabled = true;
+}
+
+std::string DefaultCoinbaseStr() {
+    CScript scriptPubKey = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
+    std::unique_ptr<CBlockTemplate> tpl(CreateNewBlock(scriptPubKey));
+    CScript coinbase = tpl->block.vtx.at(0).vin.at(0).scriptSig;
+    return std::string(coinbase.begin(), coinbase.end());
+}
+
+BOOST_AUTO_TEST_CASE(CreateNewBlock_bip100str)
+{
+    LOCK(cs_main);
+
+    // No vote defined. Should only contain EB.
+    std::string c = DefaultCoinbaseStr();
+    BOOST_CHECK(c.find("/BIP100/EB1/") != std::string::npos);
+    BOOST_CHECK(c.find("/B1/") == std::string::npos);
+
+    // Vote for 16MB blocks (using -maxblocksizevote)
+    struct Dummy : public ArgGetter {
+        int64_t GetArg(const std::string& arg, int64_t def) override {
+            return arg == "-maxblocksizevote" ? 16 : def;
+        }
+
+        bool GetBool(const std::string&, bool def) override
+        { return def; }
+
+        std::vector<std::string> GetMultiArgs(const std::string&) override
+        { return { }; }
+    };
+    auto reset = SetDummyArgGetter(std::unique_ptr<ArgGetter>(new Dummy));
+
+    c = DefaultCoinbaseStr();
+    BOOST_CHECK(c.find("/BIP100/B16/EB1/") != std::string::npos);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
