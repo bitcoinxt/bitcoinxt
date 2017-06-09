97aa677abcac6b8787f3bc5e6ee704d62634eb13
Amaury SECHET <deadalnix@gmail.com>
2017-06-10 00:00:42 +0200

    Add an utility method to check if a script is a comitment to a certain value.
    Summary: This will help implement anti replay feature.

--- src/script/script.cpp
+++ src/script/script.cpp
@@ -227,3 +227,24 @@ bool CScript::IsPushOnly() const
     }
     return true;
 }
+
+bool CScript::IsCommitment(const std::vector<unsigned char> &data) const {
+    // To ensure we have an immediate push, we limit the commitment size to 64
+    // bytes. In addition to the data themselves, we have 2 extra bytes:
+    // OP_RETURN and the push opcode itself.
+    if (data.size() > 64 || this->size() != data.size() + 2) {
+        return false;
+    }
+
+    if ((*this)[0] != OP_RETURN || (*this)[1] != data.size()) {
+        return false;
+    }
+
+    for (size_t i = 0; i < data.size(); i++) {
+        if ((*this)[i + 2] != data[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
