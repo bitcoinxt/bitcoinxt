2a7d5690e4b4646f65ef6a312922a17f991dcff7
Amaury SECHET <deadalnix@gmail.com>
2017-07-24 21:05:48 +0200

    BTC => BCC
    Summary: Update the ticker name to make it clear as to what is going on to users.

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -477,7 +477,7 @@ UniValue gettxout(const UniValue& params, bool fHelp)
             "{\n"
             "  \"bestblock\" : \"hash\",    (string) the block hash\n"
             "  \"confirmations\" : n,       (numeric) The number of confirmations\n"
-            "  \"value\" : x.xxx,           (numeric) The transaction value in btc\n"
+            "  \"value\" : x.xxx,           (numeric) The transaction value in bcc\n"
             "  \"scriptPubKey\" : {         (json object)\n"
             "     \"asm\" : \"code\",       (string) \n"
             "     \"hex\" : \"hex\",        (string) \n"
