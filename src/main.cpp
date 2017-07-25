e49f948ab44fadce0ae949c09b7faf7a67e0bdd1
Tom Harding <tomh@thinlink.com>
2017-07-24 17:21:59 -0700

    Require fork block to be >1MB

--- src/main.cpp
+++ src/main.cpp
@@ -3367,6 +3367,7 @@ bool ContextualCheckBlockHeader(const CBlockHeader& block, CValidationState& sta
 bool ContextualCheckBlock(const CBlock& block, CValidationState& state, CBlockIndex * const pindexPrev)
 {
     const int nHeight = pindexPrev == NULL ? 0 : pindexPrev->nHeight + 1;
+    const int64_t nMedianTimePastPrev = (pindexPrev == NULL ? 0 : pindexPrev->GetMedianTimePast());
     const Consensus::Params& consensusParams = Params().GetConsensus();
 
     // Start enforcing BIP113 (Median Time Past) using versionbits logic.
@@ -3375,8 +3376,23 @@ bool ContextualCheckBlock(const CBlock& block, CValidationState& state, CBlockIn
         nLockTimeFlags |= LOCKTIME_MEDIAN_TIME_PAST;
     }
 
+    const int64_t uahfTime = Opt().UAHFTime();
+    if (uahfTime != 0) {
+        // If UAHF is enabled for the current block, but not for the previous
+        // block, this is the fork block, so we must check that the block is larger than 1MB.
+        const int64_t nMTPPrevPrev = (pindexPrev == NULL || pindexPrev->pprev == NULL ? 0 :
+                                      pindexPrev->pprev->GetMedianTimePast());
+        if ((nMTPPrevPrev < uahfTime) && (uahfTime <= nMedianTimePastPrev)) {
+            const uint64_t currentBlockSize = ::GetSerializeSize(block, SER_NETWORK, PROTOCOL_VERSION);
+            if (currentBlockSize <= MAX_BLOCK_SIZE) {
+                return state.DoS(100, error("%s: UAHF fork block must exceed %d, but this block is %d bytes",
+                        __func__, MAX_BLOCK_SIZE, currentBlockSize), REJECT_INVALID, "bad-blk-too-small");
+            }
+        }
+    }
+
     int64_t nLockTimeCutoff = (nLockTimeFlags & LOCKTIME_MEDIAN_TIME_PAST)
-                              ? pindexPrev->GetMedianTimePast()
+                              ? nMedianTimePastPrev
                               : block.GetBlockTime();
 
     // Check that all transactions are finalized
