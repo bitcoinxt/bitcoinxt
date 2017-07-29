Bitcoin-ABC/bitcoin-abc

Sign using replay protection by default in raw transaction RPC
Summary: As per title.

Test Plan: Fixed all integration tests to reflect the change.

Reviewers: freetrader, sickpig, kyuupichan, awemany, #bitcoin_abc

Reviewed By: freetrader, #bitcoin_abc

Differential Revision: https://reviews.bitcoinabc.org/D310

qa/rpc-tests/bip9-softforks.py
@@ -58,7 +58,7 @@ class BIP9SoftForksTest(ComparisonTestFramework):
         return tx
 
     def sign_transaction(self, node, tx):
-        signresult = node.signrawtransaction(bytes_to_hex_str(tx.serialize()))
+        signresult = node.signrawtransaction(bytes_to_hex_str(tx.serialize()), None, None, "ALL")
         tx = CTransaction()
         f = BytesIO(hex_str_to_bytes(signresult['hex']))
         tx.deserialize(f)

bitcoinxt/bitcoinxt

qa/rpc-tests/bip9-softforks.py
@@ -60,7 +60,7 @@ class BIP9SoftForksTest(ComparisonTestFramework):
         return tx
 
     def sign_transaction(self, node, tx):
-        signresult = node.signrawtransaction(hexlify(tx.serialize()))
+        signresult = node.signrawtransaction(hexlify(tx.serialize()), None, None, "ALL")
         tx = CTransaction()
         f = cStringIO.StringIO(unhexlify(signresult['hex']))
         tx.deserialize(f)