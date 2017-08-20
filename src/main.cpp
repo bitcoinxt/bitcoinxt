5f34c74b3b1fd7db9a7b1dd2769f95330cb6fed4
Dagur Valberg Johannsson <dagurval@pvv.ntnu.no>
2017-08-20 15:48:05 -0400

    Allow regtest fork block to be <= 1MB at genesis
    Allow this regardless of block nTime

--- src/main.cpp
+++ src/main.cpp
@@ -3437,23 +3437,19 @@ bool ContextualCheckBlock(const CBlock& block, CValidationState& state, CBlockIn
         nLockTimeFlags |= LOCKTIME_MEDIAN_TIME_PAST;
     }
 
-    const int64_t uahfTime = Opt().UAHFTime();
-    if (uahfTime != 0 &&
-        !((Params().NetworkIDString() == CBaseChainParams::REGTEST) &&
-          (uahfTime == Params().GenesisBlock().nTime))) {
+    CBlockIndex* pprevprev = pindexPrev == nullptr
+        ? nullptr
+        : pindexPrev->pprev;
 
-        CBlockIndex* pprevprev = pindexPrev == nullptr
-            ? nullptr
-            : pindexPrev->pprev;
+    if (IsUAHFActivatingBlock(nMedianTimePastPrev, pprevprev)
+            && !(Params().NetworkIDString() == CBaseChainParams::REGTEST && pprevprev == nullptr)) {
 
         // If UAHF is enabled for the current block, but not for the previous
         // block, this is the fork block, so we must check that the block is larger than 1MB.
-        if (IsUAHFActivatingBlock(nMedianTimePastPrev, pprevprev)) {
-            const uint64_t currentBlockSize = ::GetSerializeSize(block, SER_NETWORK, PROTOCOL_VERSION);
-            if (currentBlockSize <= MAX_BLOCK_SIZE) {
-                return state.DoS(100, error("%s: UAHF fork block must exceed %d, but this block is %d bytes",
-                        __func__, MAX_BLOCK_SIZE, currentBlockSize), REJECT_INVALID, "bad-blk-too-small");
-            }
+        const uint64_t currentBlockSize = ::GetSerializeSize(block, SER_NETWORK, PROTOCOL_VERSION);
+        if (currentBlockSize <= MAX_BLOCK_SIZE) {
+            return state.DoS(100, error("%s: UAHF fork block must exceed %d, but this block is %d bytes",
+                    __func__, MAX_BLOCK_SIZE, currentBlockSize), REJECT_INVALID, "bad-blk-too-small");
         }
     }
 
