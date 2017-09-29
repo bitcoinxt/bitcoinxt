4ef8e00408fbde0956c97a59fe702a516f31b252
Mark Friedenbach <mark@friedenbach.org>
2017-09-29 01:48:43 -0700

    Add error string for CLEANSTACK script violation, preventing an "unknown error" if the CLEANSTACK error condition is set.

--- src/script/script_error.cpp
+++ src/script/script_error.cpp
@@ -84,6 +84,8 @@ const char* ScriptErrorString(const ScriptError serror)
             return "Public key is neither compressed or uncompressed";
         case SCRIPT_ERR_NONCOMPRESSED_PUBKEY:
             return "Using non-compressed public key";
+        case SCRIPT_ERR_CLEANSTACK:
+            return "Extra items left on stack after execution";
         case SCRIPT_ERR_ILLEGAL_FORKID:
             return "Illegal use of SIGHASH_FORKID";
         case SCRIPT_ERR_MUST_USE_FORKID:
