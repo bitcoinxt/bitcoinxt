8dfc3c52fb72a2700529871a1f134e21a23f3678
Wladimir J. van der Laan <laanwj@gmail.com>
2018-02-06 20:32:33 +0100

    http: Remove WaitExit from WorkQueue
    This function, which waits for all threads to exit, is no longer needed
    now that threads are joined instead.
    
    Signed-off-by: Wladimir J. van der Laan <laanwj@gmail.com>

--- src/httpserver.cpp
+++ src/httpserver.cpp
@@ -105,8 +105,7 @@ public:
                                  numThreads(0)
     {
     }
-    /** Precondition: worker threads have all stopped
-     * (call WaitExit)
+    /** Precondition: worker threads have all stopped (they have been joined).
      */
     ~WorkQueue()
     {
@@ -146,20 +145,6 @@ public:
         std::unique_lock<std::mutex> lock(cs);
         running = false;
         cond.notify_all();
-    }
-    /** Wait for worker threads to exit */
-    void WaitExit()
-    {
-        std::unique_lock<std::mutex> lock(cs);
-        while (numThreads > 0)
-            cond.wait(lock);
-    }
-
-    /** Return current depth of queue */
-    size_t Depth()
-    {
-        std::unique_lock<std::mutex> lock(cs);
-        return queue.size();
     }
 };
 
@@ -496,7 +481,6 @@ void StopHTTPServer()
     LogPrint(Log::HTTP, "Stopping HTTP server\n");
     if (workQueue) {
         LogPrint(Log::HTTP, "Waiting for HTTP worker threads to exit\n");
-        workQueue->WaitExit();
         for (auto& thread: g_thread_http_workers) {
             thread.join();
         }
