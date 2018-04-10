3c8ebcc54677bec94d0f8e6590783c26c948811f
Dagur Valberg Johannsson <dagurval@pvv.ntnu.no>
2018-04-10 23:49:23 +0200

    [qa] improve raises_jsonrpc error message

--- qa/rpc-tests/test_framework/util.py
+++ qa/rpc-tests/test_framework/util.py
@@ -539,7 +539,8 @@ def assert_raises_jsonrpc(code, message, fun, *args, **kwds):
         if (code is not None) and (code != e.error["code"]):
             raise AssertionError("Unexpected JSONRPC error code %i" % e.error["code"])
         if (message is not None) and (message not in e.error['message']):
-            raise AssertionError("Expected substring not found:"+e.error['message'])
+            raise AssertionError("Expected substring '%s' not found in: %s" \
+                    % (message, e.error['message']))
     except Exception as e:
         raise AssertionError("Unexpected exception raised: "+type(e).__name__)
     else:
