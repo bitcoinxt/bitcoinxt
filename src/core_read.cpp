6480daf6ab2484d918f21327a47ab82e526adc9d
Jason B. Cox <contact@jasonbcox.com>
2018-04-16 17:42:08 -0700

    Give more descriptive error when parsing partial-byte hex values
    Summary: Fixes T312.  `0x0` and `0x001` are examples of hex values that fail the script parser.  This isn't immediately obvious unless you're familiar with the code, and the script fails with a totally useless and generic error message.
    
    Test Plan: make check + make check with script_tests.json having one line changed 0x02 -> 0x2 (fails with the expected error)
    
    Reviewers: deadalnix, schancel, #bitcoin_abc
    
    Reviewed By: deadalnix, #bitcoin_abc
    
    Subscribers: teamcity
    
    Maniphest Tasks: T312
    
    Differential Revision: https://reviews.bitcoinabc.org/D1290

--- src/core_read.cpp
+++ src/core_read.cpp
@@ -62,15 +62,21 @@ CScript ParseScript(std::string s)
             // Number
             int64_t n = atoi64(*w);
             result << n;
-        }
-        else if (boost::algorithm::starts_with(*w, "0x") && (w->begin()+2 != w->end()) && IsHex(string(w->begin()+2, w->end())))
-        {
-            // Raw hex data, inserted NOT pushed onto stack:
-            std::vector<unsigned char> raw = ParseHex(string(w->begin()+2, w->end()));
-            result.insert(result.end(), raw.begin(), raw.end());
-        }
-        else if (w->size() >= 2 && boost::algorithm::starts_with(*w, "'") && boost::algorithm::ends_with(*w, "'"))
-        {
+        } else if (boost::algorithm::starts_with(*w, "0x") &&
+                   (w->begin() + 2 != w->end())) {
+            if (IsHex(std::string(w->begin() + 2, w->end()))) {
+                // Raw hex data, inserted NOT pushed onto stack:
+                std::vector<uint8_t> raw =
+                    ParseHex(std::string(w->begin() + 2, w->end()));
+                result.insert(result.end(), raw.begin(), raw.end());
+            } else {
+                // Should only arrive here for improperly formatted hex values
+                throw std::runtime_error("Hex numbers expected to be formatted "
+                                         "in full-byte chunks (ex: 0x00 "
+                                         "instead of 0x0)");
+            }
+        } else if (w->size() >= 2 && boost::algorithm::starts_with(*w, "'") &&
+                   boost::algorithm::ends_with(*w, "'")) {
             // Single-quoted string, pushed as data. NOTE: this is poor-man's
             // parsing, spaces/tabs/newlines in single-quoted strings won't work.
             std::vector<unsigned char> value(w->begin()+1, w->end()-1);
