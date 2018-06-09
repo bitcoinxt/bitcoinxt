Bitcoin-ABC/bitcoin-abc

OSX: Change app bundle name and executable name to BitcoinABC-Qt.app and change related OSX-specific handlers and Info.plist stuff to match.
Summary:
Changed the OSX app bundle and Mac DMG file to be BitcoinABC-Qt.app and
Bitcoin-ABC.dmg respectively (old one was Core's Bitcoin-Qt.app and
Bitcoin.dmg)

Note this is a cosmetic change only and is verified to not break
settings which are still saved in
~/Library/Preferences/org.bitcoin.Bitcoin-Qt

I will write another PR.. err.. diff.. that will update the Qt App Name
and Org Name to be Bitcoin ABC -- and I will also write transition
"migration" code that will copy settings from old core
settings/registry/etc (cross platform location in this case) to our own
BitcoinABC location.

Test Plan:
You need a mac. Or at least you need to cross-build for mac.  If you need help on how to do this I can provide you with scripts.

 make && make appbundle && make deploy. Verify name of app.

Also if you run it -- verify settings file is not changed.  Verify all other names are ok.

Reviewers: deadalnix, freetrader, #bitcoin_abc

Reviewed By: freetrader, #bitcoin_abc

Subscribers: schancel, zander, sickpig

Differential Revision: https://reviews.bitcoinabc.org/D510

src/qt/macnotificationhandler.mm
@@ -13,7 +13,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"org.bitcoinfoundation.Bitcoin-Qt";
+        return @"org.bitcoincash.BitcoinABC-Qt";
     } else {
         return [self __bundleIdentifier];
     }

bitcoinxt/bitcoinxt

src/qt/macnotificationhandler.mm
@@ -13,7 +13,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"org.bitcoinfoundation.Bitcoin-Qt";
+        return @"org.bitcoincash.Bitcoin-XT";
     } else {
         return [self __bundleIdentifier];
     }