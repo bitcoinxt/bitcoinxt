64944ed22455eb928cc84528ca84af9f97ba08c9
Dagur Valberg Johannsson <dagurval@pvv.ntnu.no>
2018-06-12 12:11:11 +0200

    verbose make

--- depends/packages/libsodium.mk
+++ depends/packages/libsodium.mk
@@ -1,7 +1,7 @@
 package=libsodium
 $(package)_version=1.0.16
-$(package)_download_path=https://download.libsodium.org/libsodium/releases/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
+$(package)_download_path=https://download.libsodium.org/libsodium/releases/
 $(package)_sha256_hash=eeadc7e1e1bcef09680fb4837d448fbdf57224978f865ac1c16745868fbd0533
 $(package)_dependencies=
 $(package)_config_opts=
@@ -19,7 +19,7 @@ define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
-  $(MAKE)
+  $(MAKE) V=1
 endef
 
 define $(package)_stage_cmds
