ab6edce96bfeea87df9f24622c4a8c2bcb74bd72
Amaury Séchet <deadalnix@gmail.com>
2018-07-01 02:43:55 +0200

    Add support for both way comparison for uint256/uint160
    Summary: Right now, they only compare one way, which is inconvenient.
    
    Test Plan: Added test cases to cover the new function.
    
    Reviewers: #bitcoin_abc, schancel
    
    Reviewed By: #bitcoin_abc, schancel
    
    Subscribers: teamcity
    
    Differential Revision: https://reviews.bitcoinabc.org/D1526

--- src/uint256.h
+++ src/uint256.h
@@ -46,6 +46,9 @@ public:
     friend inline bool operator==(const base_blob& a, const base_blob& b) { return a.Compare(b) == 0; }
     friend inline bool operator!=(const base_blob& a, const base_blob& b) { return a.Compare(b) != 0; }
     friend inline bool operator<(const base_blob& a, const base_blob& b) { return a.Compare(b) < 0; }
+    friend inline bool operator>(const base_blob &a, const base_blob &b) {
+        return a.Compare(b) > 0;
+    }
 
     std::string GetHex() const;
     void SetHex(const char* psz);
