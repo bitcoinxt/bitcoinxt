54b24d0c9de02cb2e584b4685c98851d64d25756
Dagur Valberg Johannsson <dagurval@pvv.ntnu.no>
2018-10-05 13:32:42 +0200

    [qa] helper function tx_grind_hash

--- qa/rpc-tests/test_framework/txtools.py
+++ qa/rpc-tests/test_framework/txtools.py
@@ -1,3 +1,4 @@
+import time
 from test_framework.cdefs import MIN_TX_SIZE, MAX_TXOUT_PUBKEY_SCRIPT
 from test_framework.mininode import CTransaction, FromHex, ToHex, CTxOut
 from test_framework.script import OP_RETURN, CScript
@@ -32,3 +33,18 @@ def bloat_raw_tx(rawtx_hex, min_size = None):
     FromHex(tx, rawtx_hex)
     bloat_tx(tx, min_size)
     return ToHex(tx)
+
+def grind_nsequence(tx):
+    MAX_ALLOWED_NSEQUENCE = 0xffffffff-1
+    tx.vin[0].nSequence += 1
+    if (tx.vin[0].nSequence >= MAX_ALLOWED_NSEQUENCE):
+        tx.vin[0].nSequence = 0
+
+def grind_opreturn(tx):
+    tx.vout.append(CTxOut(0, CScript([OP_RETURN])))
+
+def tx_grind_hash(tx, criteria, grinder = grind_nsequence):
+    tx.calc_sha256()
+    while (not criteria(tx.hash)):
+        grinder(tx)
+        tx.rehash()
