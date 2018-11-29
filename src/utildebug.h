d029821bce8a5851b4503142ec255f063ce1338a
Dagur Valberg Johannsson <dagurval@pvv.ntnu.no>
2018-11-29 11:31:00 +0100

    Add macro THROW_UNLESS
    Express assumptions in places where exceptions are handled. A less fatal
    alternative to 'assert'.

--- src/utildebug.h
+++ src/utildebug.h
@@ -1,6 +1,18 @@
 #ifndef BITCOIN_UTILDEBUG_H
 #define BITCOIN_UTILDEBUG_H
 
+#include <stdexcept>
+
 void dump_backtrace_stderr();
+
+class bad_state_error : public std::runtime_error {
+    using std::runtime_error::runtime_error; // inherit constructors
+};
+
+void throw_bad_state(const char* why,
+                            const char* func, const char* file, int line);
+
+// Can be used instead of assert where we are able to recover from an exception.
+#define THROW_UNLESS(cond) { if (!(cond)) { throw_bad_state(#cond, __func__, __FILE__, __LINE__); } }
 
 #endif
